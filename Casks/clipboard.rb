e36d07b071af11488d2c8ee20aa25a691bf3f215
Shiran Gabriel <gabriel.stcb@gmail.com>
2026-06-07 20:24:27 +0530

    Add Clipboard cask

---
+++ Casks/clipboard.rb
@@ -0,0 +1,15 @@
+cask "clipboard" do
+  version "0.1.0"
+  sha256 "1034be2c328969319ee2b95f53185789f4f42a2a1feeeff103e621deccfc2caa"
+
+  url "https://github.com/shirangabriel/clipboard/releases/download/v#{version}/Clipboard-#{version}-macos.zip"
+  name "Clipboard"
+  desc "macOS menu bar clipboard utility for history, sections, and favorites"
+  homepage "https://github.com/shirangabriel/clipboard"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Clipboard.app"
+
+  zap trash: "~/Library/Application Support/Clipboard"
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Tap
+
+Install Clipboard with:
+
+```sh
+brew tap shirangabriel/tap
+brew install --cask clipboard
+```
